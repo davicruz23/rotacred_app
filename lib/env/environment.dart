0cb9a99cbed413072bca54823f84f6a87b2b485c
Davi Cruz <davifieledeus@gmail.com>
2026-02-17 15:13:22 -0300

    https!

--- lib/env/environment.dart
+++ lib/env/environment.dart
@@ -1,4 +1,3 @@
 class Environment {
-  //static const String apiBaseUrl = 'https://api-gestao-n29u.onrender.com/api';
-  static const String apiBaseUrl = 'http://192.168.10.6:8081/api';
+  static const String apiBaseUrl = 'https://api.rotacred.tech/api';
 }
